f74b0f4334baad22b55ed39f07e5b5109b6d7f18
박진우 <jwpark@gaia3d.com>
2019-07-19 19:08:48 +0900

    교량 검색 controller, view 추가

--- honam-admin/src/main/java/honam/controller/BridgeController.java
+++ honam-admin/src/main/java/honam/controller/BridgeController.java
@@ -29,7 +29,7 @@ import honam.util.StringUtil;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RequestMapping("/")
+@RequestMapping("/bridge/")
 @Controller
 public class BridgeController {
 	
@@ -37,13 +37,51 @@ public class BridgeController {
 	private PropertiesConfig propertiesConfig;
 	@Autowired
 	private BridgeService bridgeService;
+	@Autowired
+	private BridgeService bridgeAttributeService;
 	
 	@RequestMapping(value = "list-bridge")
 	public String listBridge(HttpServletRequest request, Bridge bridge, @RequestParam(defaultValue="1") String pageNo, Model model) {
 		
 		log.info("@@ bridge = {}", bridge);
 		
+		String cesiumIonToken = propertiesConfig.getCesiumIonToken();
+		
+		model.addAttribute("cesiumIonToken", cesiumIonToken);
 		return "/bridge/list-bridge";
 	}
+	
+	/**
+	 * 검색 조건
+	 * @param brdige
+	 * @return
+	 */
+	private String getSearchParameters(PageType pageType, HttpServletRequest request, Bridge bridge) {
+		StringBuffer buffer = new StringBuffer();
+		boolean isListPage = true;
+		if(pageType.equals(PageType.MODIFY) || pageType.equals(PageType.DETAIL)) {
+			isListPage = false;
+		}
+		
+		if(!isListPage) {
+			buffer.append("pageNo=" + request.getParameter("pageNo"));
+		}
+		buffer.append("&");
+		buffer.append("searchWord=" + StringUtil.getDefaultValue(isListPage ? bridge.getSearchWord() : request.getParameter("searchWord")));
+		buffer.append("&");
+		buffer.append("searchOption=" + StringUtil.getDefaultValue(isListPage ? bridge.getSearchOption() : request.getParameter("searchOption")));
+		buffer.append("&");
+		try {
+			buffer.append("searchValue=" + URLEncoder.encode(StringUtil.getDefaultValue(
+					isListPage ? bridge.getSearchValue() : request.getParameter("searchValue")), "UTF-8"));
+		} catch(Exception e) {
+			e.printStackTrace();
+			buffer.append("searchValue=");
+		}
+		buffer.append("&");
+		buffer.append("listCounter=" + (isListPage ? bridge.getListCounter() : StringUtil.getDefaultValue(request.getParameter("listCount"))));
+		return buffer.toString();
+	}
+
 
 }
