a3b0cb5a97aa05ba3e93a1ddebbace0e0bc7359b
박진우 <jwpark@gaia3d.com>
2019-07-24 13:43:05 +0900

    교량 검색 중 주소 검색 기능 개발

--- honam-admin/src/main/java/honam/controller/BridgeController.java
+++ honam-admin/src/main/java/honam/controller/BridgeController.java
@@ -26,6 +26,8 @@ import honam.domain.PageType;
 import honam.domain.Pagination;
 import honam.util.DateUtil;
 import honam.util.StringUtil;
+import honam.domain.SkSdo;
+import honam.domain.SkSgg;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -37,8 +39,101 @@ public class BridgeController {
 	private PropertiesConfig propertiesConfig;
 	@Autowired
 	private BridgeService bridgeService;
-
 	
+	/**
+	 * 시도 목록
+	 * @return
+	 */
+	@GetMapping("sdos")
+	@ResponseBody
+	public Map<String, Object> getListSdo() {
+		Map<String, Object> map = new HashMap<>();
+		String result = "success";
+		try {
+
+			List<SkSdo> sdoList = bridgeService.getListSdoExceptGeom();
+			map.put("sdoList", sdoList);
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = "db.exception";
+		}
+		
+		map.put("result", result);
+		log.info("@@@@ sdoList = {}", map.toString());
+		return map;
+	}
+
+	/**
+	 * 시군구 목록
+	 * @param sdo_code
+	 * @return
+	 */
+	@GetMapping("sdos/{sdo_code:[0-9]+}/sggs")
+	@ResponseBody
+	public Map<String, Object> getListSggBySdo(@PathVariable String sdo_code) {
+		Map<String, Object> map = new HashMap<>();
+		String result = "success";
+		try {
+			// TODO 여기 들어 오지 않음. PathVariable 은 불칠전해서 이렇게 하고 싶음
+			if(sdo_code == null || "".equals(sdo_code)) {
+				map.put("result", "sdo.code.invalid");
+				log.info("validate error 발생: {} ", map.toString());
+				return map;
+			}
+			
+			List<SkSgg> sggList = bridgeService.getListSggBySdoExceptGeom(sdo_code);
+			map.put("sggList", sggList);
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = "db.exception";
+		}
+		
+		map.put("result", result);
+		log.info("@@@@ sggList = {}", map.toString());
+		return map;
+	}
+	
+	/**
+	 * 선택 한 위치의 center point를 구함
+	 * @param skEmd
+	 * @return
+	 */
+	@GetMapping("centroids")
+	@ResponseBody
+	public Map<String, Object> getCentroid(SkSgg skSgg) {
+		log.info("@@@@ SkSgg = {}", skSgg);
+		
+		Map<String, Object> map = new HashMap<>();
+		String result = "success";
+		try {
+			// TODO 여기 들어 오지 않음. PathVariable 은 불칠전해서 이렇게 하고 싶음
+			
+			String centerPoint = null;
+			if(skSgg.getLayer_type() == 1) {
+				// 시도
+				SkSdo skSdo = new SkSdo();
+				skSdo.setName(skSgg.getName());
+				skSdo.setBjcd(skSgg.getBjcd());
+				centerPoint = bridgeService.getCentroidSdo(skSdo);
+				log.info("@@@@ sdo center point {}", centerPoint);
+			} else if(skSgg.getLayer_type() == 2) {
+				// 시군구
+				centerPoint = bridgeService.getCentroidSgg(skSgg);
+				log.info("@@@@ sgg center point {}", centerPoint);
+			}
+			
+			String[] location = centerPoint.substring(centerPoint.indexOf("(") + 1, centerPoint.indexOf(")")).split(" "); 
+			map.put("longitude", location[0]);
+			map.put("latitude", location[1]);
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = "db.exception";
+		}
+		
+		map.put("result", result);
+		return map;
+	}
+
 	@RequestMapping(value = "list-bridge")
 	public String listBridge(HttpServletRequest request, Bridge bridge, @RequestParam(defaultValue="1") String pageNo, Model model) {
 		
@@ -47,9 +142,47 @@ public class BridgeController {
 		String cesiumIonToken = propertiesConfig.getCesiumIonToken();
 		
 		model.addAttribute("cesoumIonToken", cesiumIonToken);
-		log.info("@@ cesoumIonToken = {}", cesiumIonToken);
 		
 		return "/bridge/list-bridge";
 	}
+	
+	/**
+	 * 검색 조건
+	 * @param bridge
+	 * @return
+	 */
+//	private String getSearchParameters(PageType pageType, HttpServletRequest request, Bridge bridge) {
+//		StringBuffer buffer = new StringBuffer();
+//		boolean isListPage = true;
+//		if(pageType.equals(PageType.MODIFY) || pageType.equals(PageType.DETAIL)) {
+//			isListPage = false;
+//		}
+//		
+//		if(!isListPage) {
+//			buffer.append("pageNo=" + request.getParameter("pageNo"));
+//		}
+//		buffer.append("&");
+//		buffer.append("searchWord=" + StringUtil.getDefaultValue(isListPage ? bridge.getSearchWord() : request.getParameter("searchWord")));
+//		buffer.append("&");
+//		buffer.append("searchOption=" + StringUtil.getDefaultValue(isListPage ? bridge.getSearchOption() : request.getParameter("searchOption")));
+//		buffer.append("&");
+//		try {
+//			buffer.append("searchValue=" + URLEncoder.encode(StringUtil.getDefaultValue(
+//					isListPage ? bridge.getSearchValue() : request.getParameter("searchBalue")), "UTF-8"));
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//			buffer.append("searchValue=");
+//		}
+//		buffer.append("&");
+//		buffer.append("status=" + StringUtil.getDefaultValue(isListPage ? droneProject.getStatus() : request.getParameter("status")));
+//		buffer.append("&");
+//		buffer.append("&");
+//		buffer.append("order_word=" + StringUtil.getDefaultValue(isListPage ? droneProject.getOrder_word() : request.getParameter("order_word")));
+//		buffer.append("&");
+//		buffer.append("order_value=" + StringUtil.getDefaultValue(isListPage ? droneProject.getOrder_value() : request.getParameter("order_value")));
+//		buffer.append("&");
+//		buffer.append("list_counter=" + (isListPage ? bridge.getListCounter() : StringUtil.getDefaultValue(request.getParameter("listCount"))));
+//		return buffer.toString();
+//	}
 
 }
--- honam-admin/src/main/webapp/WEB-INF/views/bridge/list-bridge.jsp
+++ honam-admin/src/main/webapp/WEB-INF/views/bridge/list-bridge.jsp
@@ -26,7 +26,7 @@
 <body>
 <%@ include file="/WEB-INF/views/layouts/header.jsp" %>
 
-<div id="wrap" style="height:94.9%; width: 100%;">
+<div id="wrap" style="height:94.7%; width: 100%;">
 	<%@ include file="/WEB-INF/views/layouts/menu.jsp" %>
 	
 	<!-- S: 1depth / 프로젝트 목록 -->
@@ -42,7 +42,32 @@
 				
 		<div class="subContents">
 			<form:form id="searchForm" modelAttribute="bridge" method="post" action="/bridge/list-bridge" onsubmit="return searchCheck();">
-
+				<ul class="projectSearch input-group row">
+					<li class="input-set">
+						<label for="search_word">교량 명</label>
+						<form:input path="searchValue" type="search" size="25" cssClass="m" />
+						<form:hidden path="searchWord" value="brg" />
+						<form:hidden path="searchOption" value="1" />
+					</li>
+					<li class="input-set">
+						<label>주소</label>
+						<select id="sdoList" name="sdoList" class="select" style="width: 97px;">
+							<option value>시도</option>
+						</select>
+						<select id="sggList" name="sggList" class="select" style="width: 85px;">
+							<option value>시군구</option>
+						</select>					
+					</li>
+					<li class="input-set">
+						<label>관리주체</label>
+						<select id="aaa" name="sdolist" class="select" style="width: 187px;">
+							<option>전라남도</option>	
+						</select>				
+					</li>
+					<li class="input-set btn">
+						<button type="submit" value="search" class="point" id="search">검색</button>
+					</li>
+				</ul>
 			</form:form>
 			
 			
@@ -61,6 +86,7 @@
 <script type="text/javascript" src="/js/${lang}/common.js"></script>
 <script type="text/javascript" src="/js/honam-bms.js"></script>
 <script type="text/javascript" src="/js/geospatial.js"></script>
+<script type="text/javascript" src="/js/DistrictControll.js"></script>
 <script type="text/javascript">
 	// 초기 위치 설정
 	var INIT_WEST = 126.0;
@@ -68,35 +94,42 @@
 	var INIT_EAST = 130.0;
 	var INIT_NORTH = 39.0;
 	var rectangle = Cesium.Rectangle.fromDegrees(INIT_WEST, INIT_SOUTH, INIT_EAST, INIT_NORTH);
-	Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
+	Cesium.Camera.DEFAULT_VIEW_FACTOR = 1;
 	Cesium.Camera.DEFAULT_VIEW_RECTANGLE = rectangle;
 	var worldTerrain = Cesium.createWorldTerrain({
 	    requestWaterMask: false,
 	    requestVertexNormals: true
-	});
-		
+	});		
+	        
+   	Cesium.Ion.defaultAccesToken = '${cesiumIonToken}';
+   	var viewer = new Cesium.Viewer('MapContainer', {imageryProvider : imageryProvider, baseLayerPicker : true, animation:false, timeline:false, fullscreenButton:false, terrainProvider : worldTerrain});
+   	viewer.scene.globe.depthTestAgainstTerrain = false;
+    
+	DistrictControll(viewer);
+    var bridgeGroupLayers = viewer.imageryLayers;
+	bridgeGroupLayers.addImageryProvider(new Cesium.WebMapServiceImageryProvider({
+	    url : 'http://localhost:8080/geoserver/honambms/wms',
+	    layers : 'honambms:BridgeGroup',
+	    parameters : {
+			service : 'WMS'
+				,version : '1.3.0'
+				,request : 'GetMap'
+				,transparent : 'true'
+				,tiled : 'true'
+				,format : 'image/png'
+	    }
+		//,proxy: new Cesium.DefaultProxy('/proxy/')
+		,enablePickFeatures: false
+	}));
 	
-//	Cesium.Ion.defaultAccessToken = '${cesiumIonToken}';
-    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlMjc5MjdmNS05MjBjLTQ2MzEtOTQwOC1jNjVmMDA1MzlhODMiLCJpZCI6Mzk5Miwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTUzOTU4MDQ5N30.v1FiFQDcHTMyQJXxIPzxGkQYGIY6K2OAy6tRJjV57yY';
+	// TODO: policy에 추가
+	var INTERVAL_TIME = 5000; 
+	$(document).ready(function() {
+		$("#projectMenu").addClass("on");
 
-	var viewer = new Cesium.Viewer('MapContainer', {
-		terrainProvider : new Cesium.CesiumTerrainProvider({
-            url: Cesium.IonResource.fromAssetId(1)
-        }), 
-		baseLayerPicker : true, animation:false, timeline:false, fullscreenButton:false, terrainProvider : worldTerrain});
-	viewer.scene.globe.depthTestAgainstTerrain = true;
+	});
+
     
-    var tileset = viewer.scene.primitives.add(
-        new Cesium.Cesium3DTileset({
-            url: Cesium.IonResource.fromAssetId(28536)
-        })
-    );
-    
-    viewer.zoomTo(tileset)
-        .otherwise(function (error) {
-            console.log(error);
-        });
-	
 </script>
 </body>
 </html>
